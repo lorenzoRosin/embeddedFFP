faed5f7e6bd457ec80c1b7bfb695d072358767f4
MSI_Lore <rosin.lorenzo@hotmail.it>
2022-09-12 22:38:39 +0200

    Added all needed files

---
+++ C/Test/TestFunction/Inc/eFSPMsgReceiverTest.h
@@ -0,0 +1,42 @@
+/**
+ * @file eFSPDataStuffUnStuffTest.h
+ *
+ */
+
+#ifndef ECUDATASTUFFUNSTUFFTEST_H
+#define ECUDATASTUFFUNSTUFFTEST_H
+
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+/***********************************************************************************************************************
+ *      INCLUDES
+ **********************************************************************************************************************/
+#include "eFSPType.h"
+#include "eFSPByteUnStufferTest.h"
+#include "eFSPByteStuffer.h"
+
+
+
+/***********************************************************************************************************************
+ * GLOBAL PROTOTYPES
+ **********************************************************************************************************************/
+/**
+ * Test the data stuff and unstuff
+ */
+void dataStuffUnStuffTest(void);
+
+
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+
+
+#endif /* ECUDATASTUFFUNSTUFFTEST_H */
