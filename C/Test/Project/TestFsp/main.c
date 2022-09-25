d51004e21ded1eb8f0b276974a0b32285b598ecf
MSI_Lore <rosin.lorenzo@hotmail.it>
2022-09-25 21:50:13 +0200

    Cleaned up

---
+++ C/Test/Project/TestFsp/main.c
@@ -0,0 +1,22 @@
+#include "eFSPMsgEncoderTest.h"
+// #include "eFSPMsgDecoderTest.h"
+// #include "eFSPMsgDeEncoderTest.h"
+// #include "eFSPMsgReceiverTest.h"
+// #include "eFSPMsgRxTransmitterTest.h"
+// #include "eFSPMsgTransmitterTest.h"
+
+int main(void);
+
+
+int main(void)
+{
+    /* Start testing */
+    msgEncoderTest();
+    // msgDecoderTest();
+    // msgDeEncoderTest();
+    // msgReceiverTest();
+    // msgTransmitterTest();
+    // msgRxTransmitterTest();
+
+    return 0;
+}
