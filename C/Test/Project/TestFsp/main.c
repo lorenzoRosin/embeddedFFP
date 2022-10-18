ed5d9d7efb3a992e62e1820a35118b4b73d55b66
MSI_Lore <rosin.lorenzo@hotmail.it>
2022-10-18 21:53:07 +0200

    Start MESSAGE ENCODER TEST

--- C/Test/Project/TestFsp/main.c
+++ C/Test/Project/TestFsp/main.c
@@ -11,8 +11,8 @@ int main(void);
 int main(void)
 {
     /* Start testing */
-    //msgEncoderTest();
-    msgDecoderTest();
+    msgEncoderTest();
+    // msgDecoderTest();
     // msgDeEncoderTest();
     // msgReceiverTest();
     // msgTransmitterTest();
