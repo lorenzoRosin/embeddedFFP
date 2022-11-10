e59ab9f91395c296934dc08b8ef9a942a6439305
MSI_Lore <rosin.lorenzo@hotmail.it>
2022-11-10 21:54:26 +0100

    Test Update

--- C/Test/Code/Src/eFSPMsgDecoderTest.c
+++ C/Test/Code/Src/eFSPMsgDecoderTest.c
@@ -19,7 +19,7 @@
  **********************************************************************************************************************/
 typedef struct
 {
-    e_eCU_Crc_Res lastError;
+    e_eCU_CRC_Res lastError;
 }s_eCU_crcAdapterCtx;
 
 static bool_t c32SAdapt(void* cntx, const uint32_t s, const uint8_t d[], const uint32_t dLen, uint32_t* const c32Val);
@@ -88,7 +88,7 @@ bool_t c32SAdapt(void* cntx, const uint32_t s, const uint8_t d[], const uint32_t
     {
         ctxCur = (s_eCU_crcAdapterCtx*)cntx;
 
-        ctxCur->lastError = crc32Seed(s, (const uint8_t*)d, dLen, c32Val);
+        ctxCur->lastError = CRC_32Seed(s, (const uint8_t*)d, dLen, c32Val);
         if( CRC_RES_OK == ctxCur->lastError )
         {
             result = true;
