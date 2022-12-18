4c39fded02ffc503a7007514adcc7c2bf90f74a3
MSI_Lore <rosin.lorenzo@hotmail.it>
2022-12-18 19:07:07 +0100

    Update

--- C/Code/Inc/eFSPStandardType.h
+++ C/Code/Inc/eFSPStandardType.h
@@ -23,26 +23,6 @@ extern "C" {
 
 
 
-/***********************************************************************************************************************
- *      DEFINES
- **********************************************************************************************************************/
-#ifndef NULL
-  #define NULL          ( _NULL )
-#endif
-
-#ifdef __IAR_SYSTEMS_ICC__
-    #pragma cstat_disable = "MISRAC2004-19.4"
-    /* Suppressed for code clarity in test execution*/
-#endif
-
-    #ifndef bool_t
-        #define bool_t           bool
-    #endif
-
-#ifdef __IAR_SYSTEMS_ICC__
-    #pragma cstat_restore = "MISRAC2004-19.4"
-#endif
-
 #ifdef __cplusplus
 } /* extern "C" */
 #endif
