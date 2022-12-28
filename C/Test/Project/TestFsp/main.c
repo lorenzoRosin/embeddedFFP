2ca2c466b0a92514f219cf0058b7c6fdbcbb3cc1
MSI_Lore <rosin.lorenzo@hotmail.it>
2022-12-28 19:50:50 +0100

    Full testing

--- C/Test/Project/TestFsp/main.c
+++ C/Test/Project/TestFsp/main.c
@@ -12,11 +12,11 @@ int main(void);
 int main(void)
 {
     /* Start testing */
-    //  eFSP_MSGETST_ExeTest();
-    //  eFSP_MSGDTST_ExeTest();
-    //  eFSP_MSGDMSGETST_ExeTest();
-    //  eFSP_MSGTXTST_ExeTest();
-    //  eFSP_MSGRXTST_ExeTest();
+    eFSP_MSGETST_ExeTest();
+    eFSP_MSGDTST_ExeTest();
+    eFSP_MSGDMSGETST_ExeTest();
+    eFSP_MSGTXTST_ExeTest();
+    eFSP_MSGRXTST_ExeTest();
     eFSP_MSGRXMSGTXTST_ExeTest();
 
     return 0;
